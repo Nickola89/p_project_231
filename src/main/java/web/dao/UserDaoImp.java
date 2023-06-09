6124fad7310dff0e1a965d4255a31d2749e7620b
Nickolay Artemyev <Artemyev1000@gmail.com>
2023-06-09 16:33:16 +0900

    commit 2

--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -1,12 +1,11 @@
 package web.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
